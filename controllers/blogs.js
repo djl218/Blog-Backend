f037c051ea27793e1c4dee4ceff170c10441d5f9
Daniel Leskosky <dleskosky@gmail.com>
2020-07-12 18:29:32 -0400

    Commit for exercise 4.2

---
+++ controllers/blogs.js
@@ -0,0 +1,26 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs.map(blog => blog.toJSON()))
+    })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+    const body = request.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    })
+
+    blog.save().then(savedBlog => {
+        response.json(savedBlog.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const Blog = require('./models/blog')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const cors = require('cors')
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('build'))
-
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response, next) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then(result => {
-        response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
