c1c551fc32839c2d2f2a16a9aee2fae85d38e894
Daniel Leskosky <dleskosky@gmail.com>
2020-07-17 22:05:51 -0400

    Commit for exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
-const dummy = (blogs) => {
+const dummy = (array) => {
     return 1
 }
 
+const totalLikes = (array) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+
+    return array.length === 0
+        ? 0
+        : array.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
