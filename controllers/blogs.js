e1b9eb39f3e59711b060487623b3c7a4ca02d308
Daniel Leskosky <dleskosky@gmail.com>
2020-07-26 15:28:59 -0400

    Commit for exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,31 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+    
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
+    const user = await User.findById(body.userId)
+
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes,
+        user: user._id
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.json(savedBlog)
 })
 
--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,9 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+    .find({}).populate('blogs', { title: 1,  author: 1, url: 1, likes: 1 })
+
     response.json(users)
 })
 
