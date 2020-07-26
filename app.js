c02d4e682f6ef5c3f047d70d9dbcda4e222f617d
Daniel Leskosky <dleskosky@gmail.com>
2020-07-26 16:05:08 -0400

    Commit for exercise 4.18

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('express-async-errors')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -26,6 +27,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
  app.use('/api/blogs', blogsRouter)
  app.use('/api/users', usersRouter)
+ app.use('/api/login', loginRouter)
 
  app.use(middleware.unknownEndpoint)
  app.use(middleware.errorHandler)
