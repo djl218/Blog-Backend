a8a7b897d2ab1f2186a43fa65603c8c1edbce694
Daniel Leskosky <dleskosky@gmail.com>
2020-07-26 14:09:39 -0400

    Commit for exercise 4.15

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 require('express-async-errors')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -24,6 +25,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
  app.use(middleware.requestLogger) 
 
  app.use('/api/blogs', blogsRouter)
+ app.use('/api/users', usersRouter)
 
  app.use(middleware.unknownEndpoint)
  app.use(middleware.errorHandler)
--- models/blog.js
+++ models/blog.js
@@ -10,7 +10,11 @@ const blogSchema = new mongoose.Schema({
       required: true
     },
     url: String,
-    likes: Number
+    likes: Number,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
 })
 
 blogSchema.set('toJSON', {
