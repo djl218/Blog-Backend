0cbaa53adc3bdbb373b342fe276e76241ade23c2
Daniel Leskosky <dleskosky@gmail.com>
2020-07-27 05:57:30 -0400

    Commit for exercise 4.20

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
  app.use(cors())
  app.use(express.static('build'))
  app.use(express.json())
- app.use(middleware.requestLogger) 
+ app.use(middleware.requestLogger)
+ app.use(middleware.tokenExtractor) 
 
  app.use('/api/blogs', blogsRouter)
  app.use('/api/users', usersRouter)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,6 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
     .find({}).populate('user', { username: 1, name: 1 })
@@ -20,10 +12,9 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const token = getTokenFrom(request)
 
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)   
+    if (!request.token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
     }
 
