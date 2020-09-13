147f0c8d661e995c35ab036c3271fde0ac763ac5
Daniel Leskosky <dleskosky@gmail.com>
2020-09-12 23:39:31 -0400

    uibuild

---
+++ build/precache-manifest.1b00d04129ba882189086666e9d4aa9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d97ed1415f2d2620a6427b3679911e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f136de36458fb627ba38",
+    "url": "/static/js/2.3fe34444.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3fe34444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39809ffba62513f91ce9",
+    "url": "/static/js/main.4af04a7c.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
